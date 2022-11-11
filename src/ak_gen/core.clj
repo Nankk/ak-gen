b9ea9fc0aa6ffd6d60e430aa1fe3410693c8dead
Nankk Home <toc32768@yahoo.co.jp>
2022-11-12 04:20:22 +0900

    :bug: Fix modifiers
    入力Modifierと出力Modifierがごっちゃになっていたので修正

--- src/ak_gen/core.clj
+++ src/ak_gen/core.clj
@@ -5,32 +5,32 @@
             [clojure.java.io :as io]
             [clojure.math.combinatorics :as combo]
             [clojure.string :as str]
-            [environ.core :refer [env]]
-            [ak-gen.template :as template]))
+            [environ.core :refer [env]]))
 
 (defn modkey->str [modkey]
   (str "<" (name modkey) ">"))
 
-(defn ->script [{:as hotkey-info :keys [modifiers keys]}]
+(defn ->script [{:as hotkey-info :keys [send-modifiers keys]}]
   (str "keyboard.send_keys(\""
-       (when-not (empty? modifiers)
-         (str (str/join "+" (map modkey->str modifiers)) "+"))
+       (when-not (empty? send-modifiers)
+         (str (str/join "+" (map modkey->str send-modifiers)) "+"))
        keys
        "\")"))
 
 (defn write-script [{:as hotkey-info :keys [dirpath basename]}]
   (let [p       (path/join dirpath (str basename ".py"))
         content (->script hotkey-info)]
+    (println p)
     (spit p content)))
 
-(defn ->hotkey-settings [{:as hotkey-info :keys [modifiers hotkey]}]
-  {:description    (str (str/join "_" (map name modifiers)) "_" hotkey)
+(defn ->hotkey-settings [{:as hotkey-info :keys [modifiers send-modifiers hotkey]}]
+  {:description    (str (str/join "_" (map name send-modifiers)) "_" hotkey)
    :store          {}
    :showInTrayMenu false
    :type           "script"
    :modes          [3]
    :usageCount     0
-   :hotkey         {:modifiers (map modkey->str modifiers) :hotKey hotkey}
+   :hotkey         {:modifiers (map modkey->str (concat modifiers send-modifiers)) :hotKey hotkey}
    :prompt         false
    :filter         {:regex nil :isRecursive false}
    :omitTrigger    false
@@ -46,14 +46,14 @@
         content (json/write-str (->hotkey-settings hotkey-info))]
     (spit p content)))
 
-(defn write-hotkey-file [{:as hotkey-info :keys [modifiers hotkey keys blind? dirpath]}]
+(defn write-hotkey-file [{:as hotkey-info :keys [modifiers send-modifiers hotkey keys blind? dirpath]}]
   (if blind?
     (doseq [mods (combo/subsets [:ctrl :shift :alt])]
       (write-hotkey-file (-> hotkey-info
                              (assoc ,, :blind? false)
-                             (update ,, :modifiers #(vec (set (concat mods %)))))))
-    (let [basename (str (when (not-empty modifiers)
-                          (str (str/join "_" (map name modifiers)) "_"))
+                             (assoc ,, :send-modifiers (vec (set mods))))))
+    (let [basename (str (when (not-empty send-modifiers)
+                          (str (str/join "_" (map name send-modifiers)) "_"))
                         hotkey)]
       (write-script (assoc hotkey-info :basename basename))
       (write-hotkey-settings (assoc hotkey-info :basename basename)))))
@@ -108,4 +108,4 @@
      :hotkey "i"
      :keys "<up>"
      :blind? true}])
-  (-main "/home/shimotoku/tmp/ak-gen-input.csv"))
+  (-main "/home/nankk/tmp/autokey/input/ak-gen-input.csv"))
